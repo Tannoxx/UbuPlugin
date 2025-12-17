a3483ad15ad3433c13d9700e52e90d369a67bb29
Tannoxx <tomtom.bacouet@gmail.com>
2025-12-17 14:05:12 +0100

    Add files via upload

---
+++ UbuPlugin/src/main/java/fr/tannoxx/ubuplugin/modules/ranks/RanksModule.java
@@ -0,0 +1,77 @@
+package fr.tannoxx.ubuplugin.modules.ranks;
+
+import fr.tannoxx.ubuplugin.UbuPlugin;
+import fr.tannoxx.ubuplugin.common.module.Module;
+import fr.tannoxx.ubuplugin.common.module.ModuleManager;
+import fr.tannoxx.ubuplugin.modules.ranks.commands.*;
+import fr.tannoxx.ubuplugin.modules.ranks.data.RankDataManager;
+import fr.tannoxx.ubuplugin.modules.ranks.listeners.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+/**
+ * Module gérant le système de ranks et le chat
+ * Thread-safe avec RankDataManager
+ *
+ * @author Tannoxx
+ * @version 2.0.0
+ */
+public class RanksModule extends Module {
+
+    private RankDataManager rankDataManager;
+
+    public RanksModule(@NotNull UbuPlugin plugin, @NotNull ModuleManager moduleManager) {
+        super(plugin, moduleManager);
+    }
+
+    @Override
+    public void onLoad() {
+        super.onLoad();
+        // Initialiser le gestionnaire de données
+        rankDataManager = new RankDataManager(this);
+    }
+
+    @Override
+    public void onEnable() {
+        // Créer les tables si nécessaire
+        rankDataManager.createTables();
+
+        // Enregistrer les listeners
+        plugin.getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), plugin);
+        plugin.getServer().getPluginManager().registerEvents(new PlayerQuitListener(this), plugin);
+        plugin.getServer().getPluginManager().registerEvents(new ChatListener(this), plugin);
+        plugin.getServer().getPluginManager().registerEvents(new MuteListener(this), plugin);
+
+        // Enregistrer les commandes
+        Objects.requireNonNull(plugin.getCommand("rank")).setExecutor(new RankCommand(this));
+        Objects.requireNonNull(plugin.getCommand("prefix")).setExecutor(new PrefixCommand(this));
+        Objects.requireNonNull(plugin.getCommand("mute")).setExecutor(new MuteCommand(this));
+        Objects.requireNonNull(plugin.getCommand("unmute")).setExecutor(new UnmuteCommand(this));
+
+        info("Module Ranks activé");
+    }
+
+    @Override
+    public void onDisable() {
+        // Nettoyer les teams
+        if (rankDataManager != null) {
+            rankDataManager.cleanup();
+        }
+        info("Module Ranks désactivé");
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return "Ranks";
+    }
+
+    @NotNull
+    public RankDataManager getRankDataManager() {
+        if (rankDataManager == null) {
+            throw new IllegalStateException("RankDataManager non initialisé");
+        }
+        return rankDataManager;
+    }
+}
